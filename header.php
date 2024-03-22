e144da04f223060626540e1055c20b19e89b8d9b
fazarath <fazarath.emedia@gmail.com>
2024-03-22 19:25:15 +0530

    Implemented User Acccounts UIs

--- header.php
+++ header.php
@@ -26,8 +26,6 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Settings</a></li>
-                        <li><hr class="dropdown-divider" /></li>
                         <li><a class="dropdown-item" href="login.html">Logout</a></li>
                     </ul>
                 </li>
@@ -43,6 +41,17 @@
                                 <div class="sb-nav-link-icon"><i href="add-apartment-complex.php" class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
+                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseUserAccounts" aria-expanded="false" aria-controls="collapseUserAccounts">
+                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                                User Accounts
+                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+                            </a>
+                            <div class="collapse" id="collapseUserAccounts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+                                <nav class="sb-sidenav-menu-nested nav">
+                                    <a class="nav-link" href="add-user-accounts.php">Add User Accounts</a>
+                                    <a class="nav-link" href="user-accounts.php">List User Accounts</a>
+                                </nav>
+                            </div>
                             <div class="sb-sidenav-menu-heading">Apartment</div>
                             <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseComplex" aria-expanded="false" aria-controls="collapseComplex">
                                 <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
