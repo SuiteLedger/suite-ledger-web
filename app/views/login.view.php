0f36e7ffb6687901c1694f04f9d3df4145b94da1
= <nniroshan@palo-it.com>
2024-03-28 08:52:56 +0800

    user creation and user login

--- app/views/login.view.php
+++ app/views/login.view.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content=""/>
     <meta name="author" content=""/>
-    <title>Login - SB Admin</title>
+    <title><?=$pageTitle . ' | ' . APP_NAME ?></title>
     <link href="<?= ROOT_DIRECTORY ?>/assets/css/styles.css" rel="stylesheet"/>
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
@@ -20,19 +20,19 @@
                         <div class="card shadow-lg border-0 rounded-lg mt-5">
                             <div class="card-header"><h3 class="text-center font-weight-light my-4">Login</h3></div>
                             <div class="card-body">
-                                <form id="loginForm">
+                                <form id="loginForm" method="post" action="<?= ROOT_DIRECTORY . PAGE_URL_LOGIN ?>">
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputEmail" type="email"
+                                        <input class="form-control" id="inputEmail" type="email" name="email"
                                                placeholder="name@example.com" required>
                                         <label for="inputEmail">Email address</label>
                                     </div>
                                     <div class="form-floating mb-3">
-                                        <input class="form-control" id="inputPassword" type="password"
+                                        <input class="form-control" id="inputPassword" type="password" name="password"
                                                placeholder="Password" required>
                                         <label for="inputPassword">Password</label>
                                     </div>
                                     <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-                                        <a class="small" href="homepage.html">Go to Homepage!</a>
+                                        <a class="small" href="<?= ROOT_DIRECTORY ?>">Go to Homepage!</a>
                                         <button class="btn btn-primary" type="submit">Login</button>
                                     </div>
                                 </form>
