21bcde64c4a169d163a4a929503fe5fd452b4d28
fazarath <fazarath.emedia@gmail.com>
2024-04-02 19:34:28 +0530

    Updated Header File to fix incorrect expand

--- app/views/includes/header.view.php
+++ app/views/includes/header.view.php
@@ -154,13 +154,13 @@
                         </div>
 
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse"
-                           data-bs-target="#collapseUserAccounts" aria-expanded="false"
-                           aria-controls="collapseUserAccounts">
+                           data-bs-target="#collapseUserRoles" aria-expanded="false"
+                           aria-controls="collapseUserRoles">
                             <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                             User Roles
                             <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                         </a>
-                        <div class="collapse" id="collapseUserAccounts" aria-labelledby="headingOne"
+                        <div class="collapse" id="collapseUserRoles" aria-labelledby="headingOne"
                              data-bs-parent="#sidenavAccordion">
                             <nav class="sb-sidenav-menu-nested nav">
                                 <a class="nav-link" href="<?= ROOT_DIRECTORY ?>/userRole/list">List User Roles</a>
